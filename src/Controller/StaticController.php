5ddb50121894a367d3a4f4b9fd0804440594b1dc
Andrew Yastin <yastin@ukr.net>
2024-06-17 16:11:46 +0300

    initial

--- src/Controller/StaticController.php
+++ src/Controller/StaticController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\MailerInterface;
 
 class StaticController extends AbstractController
 {
@@ -13,6 +16,33 @@ class StaticController extends AbstractController
     {
         return $this->render('base.html.twig', [
             'controller_name' => 'IndexController',
+        ]);
+    }
+
+    #[Route('/mailtest', name: 'mailtest')]
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('yastin@ukr.net')
+            ->to('andrew.yastin@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!');
+            //->html('<p>See Twig integration for better HTML integration!</p>');
+
+        try {
+            $mailer->send($email);
+        } catch (TransportExceptionInterface $e) {
+            dump($e);
+            // some error prevented the email sending; display an
+            // error message or try to resend the message
+        }
+        dd('here');
+        return $this->render('base.html.twig', [
+            'controller_name' => 'mailtest',
         ]);
     }
 }
